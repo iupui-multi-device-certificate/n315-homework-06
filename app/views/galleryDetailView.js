133efee0a36aa48f551041ee6184399ae246596d
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-03-16 16:37:20 -0400

    feature: start style detail view

--- app/views/galleryDetailView.js
+++ app/views/galleryDetailView.js
@@ -1,12 +1,13 @@
 export const galleryDetailView = (campingItem) => `
   <article class="gallery-detail content" id=${campingItem.id}>
-    <img src="./images/camping/full/${campingItem.fullImg}" alt="${campingItem.name}"/>    
-    <h1>${campingItem.name}</h1>
-    <p><span">Category:</span> ${campingItem.category}</p>  
-    <p"><span">Manufacturer:</span> ${campingItem.manufacturer}</p>     
-    <p><span">Rating:</span> ${campingItem.rating} / 5</p>
-    <p>$${campingItem.cost}</p>
-    <p>${campingItem.detail}</p>
-    
+    <img src="./images/camping/full/${campingItem.fullImg}" alt="${campingItem.name}"/>
+    <div class="detail-text">    
+      <h1>${campingItem.name}</h1>
+      <p class="cost">$${campingItem.cost}</p>
+      <p><span>Category:</span> ${campingItem.category}</p>  
+      <p><span>Manufacturer:</span> ${campingItem.manufacturer}</p>     
+      <p><span>Rating:</span> ${campingItem.rating} / 5</p>
+      <p>${campingItem.detail}</p>
+    </div>
   </article>
 `;
