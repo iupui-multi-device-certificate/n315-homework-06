c95972a920312c0f396373f1392eed08dd00d16c
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-03-17 00:05:43 -0400

    fix: detail styling to be more parallel to gallery

--- app/views/galleryDetailView.js
+++ app/views/galleryDetailView.js
@@ -4,12 +4,15 @@ export const galleryDetailView = (campingItem) => `
   <article class="gallery-detail content" id=${campingItem.id}>
     <img src="./images/camping/full/${campingItem.fullImg}" alt="${campingItem.name}"/>
     <div class="detail-text">    
-      <h1>${campingItem.name}</h1>
-      <p class="cost">$${campingItem.cost}</p>
-      <p><span>Category:</span> ${campingItem.category}</p>  
-      <p><span>Manufacturer:</span> ${campingItem.manufacturer}</p>     
-      <p><span>Rating:</span> ${campingItem.rating} / 5</p>
+      <h1 class="detail-title">${campingItem.name}</h1>     
+
       <p>${campingItem.detail}</p>
+
+      <div class="info">
+        <p><span>Manufacturer:</span> ${campingItem.manufacturer} | <span>Category:</span> ${campingItem.category}</p>
+        <p><span>Rating:</span> ${campingItem.rating} / 5</p>
+      </div> 
+      <p class="cost">$${campingItem.cost}</p>          
     </div>
   </article>
 `;
